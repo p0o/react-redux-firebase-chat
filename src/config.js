edbe64ddb1cdd3bba4084664d06a3908f78c1b00
p0o <pooria_az@ymail.com>
2016-05-21 22:41:37 +0800

    add firebase config right from firebase console

---
+++ src/config.js
@@ -0,0 +1,7 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAlEJt50TmWW__fyavtDk7ByiWrz6m9FaM",
+  authDomain: "react-redux-firebase-chat.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-chat.firebaseio.com",
+  storageBucket: "react-redux-firebase-chat.appspot.com"
+};
