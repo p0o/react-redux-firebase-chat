439d588ccf1f991b9319f9e0a43e7def4a38591d
p0o <pooria_az@ymail.com>
2016-05-21 21:18:07 +0800

    add basic react component

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-require('../styles/index.scss');
-document.write(require('./test'));
+import React from "react";
+import ReactDOM from "react-dom";
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello { this.props.name }</div>;
+  }
+});
+
+ReactDOM.render(<HelloMessage name="Pooria" />, document.querySelector("#react-container"));
