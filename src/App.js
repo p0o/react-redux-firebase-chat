eca850917ce39903ccb00aa831d67c27af1b64a8
p0o <pooria_az@ymail.com>
2016-06-13 21:36:10 +0800

    rename file to App.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { firebaseConfig } from './config';
+
+if (window.firebase) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+  }
+
+  handleSignin() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('email');
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      const { accessToken } = result.credential;
+      const { user } = result;
+      this.setState({ name: user.displayName });
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <div>{ name? `Hello ${ name }`: '' }</div>
+        <p><a href="#" onClick={ () => this.handleSignin() }>Sign in here</a></p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#react-container"));
