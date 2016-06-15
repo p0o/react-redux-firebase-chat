9e6533d304ac2469b2e9b1b836d3d05929128767
p0o <pooria_az@ymail.com>
2016-06-15 17:22:12 +0800

    import necessary stuff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { render } from 'react-dom';
+import App from './containers/App';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
