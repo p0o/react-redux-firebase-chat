2eafec457947522edd59ddba47fb07b25462e89b
p0o <pooria_az@ymail.com>
2016-06-19 20:00:28 +0800

    add redux thunk for asynchronous requests

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import { render } from 'react-dom';
 import App from './app/App';
 import reducers from './app/reducers';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'thunk';
 import { firebaseConfig } from './config';
 
 if (window.firebase) {
   firebase.initializeApp(firebaseConfig);
 }
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
 
 // Dispatch initialization action here
 
