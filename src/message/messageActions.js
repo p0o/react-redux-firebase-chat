b8f88acf5490a986cb7ce3011609735b8a48f9d9
p0o <pooria_az@ymail.com>
2016-06-23 00:13:43 +0800

    create thunk function to be used for Firebase

--- src/message/messageActions.js
+++ src/message/messageActions.js
@@ -1,6 +1,6 @@
 import * as types from './messageActionTypes';
 
-export const sendMessage = (uid, message, createdAt) => {
+export const sendMessageInProgress = (uid, message, createdAt) => {
   return {
     type: types.SEND_MESSAGE,
     uid,
@@ -18,5 +18,17 @@ export const sendMessageSuccess = () => {
 export const sendMessageError = () => {
   return {
     type: types.SEND_MESSAGE_ERROR
+  }
+};
+
+export const sendMessage = (message) => {
+  return (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (uid !== 0) {
+      dispatch( sendMessageInProgress(uid, message, Date.now()) );
+      // TODO: add firebase stuff
+    } else {
+      dispatch( sendMessageError() );
+    }
   }
 };
