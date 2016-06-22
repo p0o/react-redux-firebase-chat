5e38fe8894744947f50cf095a9f84d7f476a772a
p0o <pooria_az@ymail.com>
2016-06-22 18:05:52 +0800

    write reducers for userMessage

---
+++ src/message/messageReducers.js
@@ -0,0 +1,50 @@
+import * as types from './messageActionTypes';
+
+export const userMessageItems = (state, action) => {
+  switch(action.type) {
+    case types.SEND_MESSAGE:
+      const { uid, message, createdAt } = action;
+      return [
+        ...state,
+        {
+          uid,
+          message,
+          createdAt
+        }
+      ];
+    case types.SEND_MESSAGE_SUCCESS:
+    case types.SEND_MESSAGE_ERROR:
+    default:
+      return state;
+  }
+};
+
+const userMessageInitialState = {
+  isSending: false,
+  isTyping: false,
+  items: []
+};
+
+export const userMessage = (state = userMessageInitialState, action) => {
+  switch(action.type) {
+    case types.SEND_MESSAGE:
+      return {
+        ...state,
+        isSending: true,
+        items: userMessageItems(state.items, action)
+      };
+    case types.SEND_MESSAGE_SUCCESS:
+      return {
+        ...state,
+        isSending: false
+      };
+    case types.SEND_MESSAGE_ERROR:
+      return {
+        ...state,
+        hasError: true,
+        errorMessage: action.errorMessage
+      };
+    default:
+      return state;
+  }
+};
