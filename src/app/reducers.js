56aedb5f32b0e106bb5da214f489829d76740a78
p0o <pooria_az@ymail.com>
2016-06-23 00:02:45 +0800

    fix typo

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -4,5 +4,5 @@ import * as messageReducers from './../message/messageReducers';
 
 export default combineReducers({
   auth: authReducers.auth,
-  userMessage: messageReducer.userMessage
+  userMessage: messageReducers.userMessage
 });
