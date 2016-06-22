860f0d430249b844262c64460619486f94f944cf
p0o <pooria_az@ymail.com>
2016-06-22 23:18:29 +0800

    composite userMessage with app reducers

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import * as authReducers from './../auth/authReducers';
+import * as messageReducers from './../message/messageReducers';
 
 export default combineReducers({
-  auth: authReducers.auth
+  auth: authReducers.auth,
+  userMessage: messageReducer.userMessage
 });
