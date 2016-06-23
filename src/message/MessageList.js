47e106d7374937ff6f9469ff7e1a27d25311020f
p0o <pooria_az@ymail.com>
2016-06-23 23:24:40 +0800

    display messages from state

--- src/message/MessageList.js
+++ src/message/MessageList.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { retrieveMessage } from './messageActions';
 
 export default class MessageList extends Component {
   componentDidMount() {
     this._firebaseRef = firebase.database().ref('messages');
     this._firebaseRef.on('child_added', (snapshot) => {
-      console.log(snapshot.val());
+      const { uid, message } = snapshot.val();
+      this.props.dispatch( retrieveMessage({ uid, message }) );
     });
   }
 
   render() {
+    const { messages } = this.props;
+
     return (
       <ul>
-
+        { messages.map(msg => <li>{ msg.uid + ': ' + msg.message }</li>)}
       </ul>
     );
   }
