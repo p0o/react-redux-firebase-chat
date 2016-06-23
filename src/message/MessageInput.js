b34f35f17695f8f487fee2ce9dad5b008f387898
p0o <pooria_az@ymail.com>
2016-06-23 12:13:01 +0800

    send textbox value to the action using ref API

--- src/message/MessageInput.js
+++ src/message/MessageInput.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { sendMessage } from './messageActions';
 
+let myInput;
+
 export default ({ dispatch }) => {
   return (
-    <input type="text" onKeyUp={ ({ keyCode }) => {
-      if(keyCode === 13) {
-        dispatch(sendMessage('hey'));
-      }
-    }}/>
+    <input
+      type="text"
+      ref={ (ref) => myInput = ref }
+      onKeyUp={ ({ keyCode }) => {
+        if(keyCode === 13) {
+          dispatch(sendMessage(myInput.value));
+        }
+      } }
+    />
   );
 };
