2261ac96a1a62a1e31d9b2a83545ca048d06b2a1
p0o <pooria_az@ymail.com>
2016-06-25 01:28:37 +0800

    store all the user payload instead of individual fields

--- src/user/userReducers.js
+++ src/user/userReducers.js
@@ -3,15 +3,12 @@ import * as types from './usersActionTypes';
 export const users = (state = {}, action) => {
   switch(action.type) {
     case types.USER_LOGGED_IN:
-      const { uid, email, photoURL, displayName, lastTimeLoggedIn } = action;
+      const { uid, userPayload } = action;
 
       return {
         ...state,
         [uid]: {
-          email,
-          photoURL ,
-          displayName,
-          lastTimeLoggedIn
+          ...userPayload
         }
       };
     default:
