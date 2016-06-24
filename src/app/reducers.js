8847ca6a34960a8f39f932494058a036a6c883d6
p0o <pooria_az@ymail.com>
2016-06-25 01:27:58 +0800

    add users reducer to the main state

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import * as authReducers from './../auth/authReducers';
 import * as messageReducers from './../message/messageReducers';
+import * as usersReducers from './../user/userReducers';
 
 export default combineReducers({
   auth: authReducers.auth,
   userMessage: messageReducers.userMessage,
-  messages: messageReducers.messages
+  messages: messageReducers.messages,
+  users: usersReducers.users
 });
