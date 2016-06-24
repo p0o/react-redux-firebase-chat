bbce6f7e62e94c23249dd63dec8acae1d6f24e38
p0o <pooria_az@ymail.com>
2016-06-25 00:22:09 +0800

    dispatch the action to store user into state

--- src/user/UsersList.js
+++ src/user/UsersList.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { addConnectedUser } from './usersActions';
 
 export default class UsersList extends Component {
   componentDidMount() {
     firebase.database().ref('users').on('child_added', (snapshot) => {
-
+      this.props.dispatch(
+        addConnectedUser({
+          uid: snapshot.key,
+          userPayload: snapshot.val()
+        })
+      );
     });
   }
 
